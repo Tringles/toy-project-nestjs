c8ae18493d47f0d14ef3a92602f3cc780b7e253a
Tringles <jjmjm2002@naver.com>
2022-02-27 00:58:20 +0900

    chore: adjust indent convention

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
