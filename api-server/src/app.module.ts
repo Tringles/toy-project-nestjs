363dfddb8c06295e1f8c41df47c35b8e3374c38f
Tringles <jjmjm2002@naver.com>
2022-02-28 00:46:43 +0900

    [BE] fix change postgresql to mysql

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot(typeORMConfig)
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: 'localhost',
+			port: 3306,
+			username: 'root',
+			password: 'root',
+			database: 'toy',
+			synchronize: false
+		})
 	],
 	controllers: [AppController],
 	providers: [AppService],
