7e2239a752cbc2d73de2c3c311bd63d56554789f
Tringles <jjmjm2002@naver.com>
2022-02-28 22:03:55 +0900

    feat: add getUser method

--- api-server/src/user/user.repository.ts
+++ api-server/src/user/user.repository.ts
@@ -4,12 +4,16 @@ import { User } from "./entity/user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
-
     async createUser(kakaoId: number, nickname: string, profileImage: string, email: string): Promise<User> {
         console.log(`[DB] Create User ${kakaoId} ${nickname} ${profileImage} ${email}`);
 
-        const user = this.create({kakaoId, nickname, profileImage, email});
+        const user = this.create({ kakaoId, nickname, profileImage, email });
         await this.save(user);
         return user;
+    }
+
+    async getUser(kakaoId: number): Promise<User> {
+        console.log(`[DB] Get User ${kakaoId}`);
+        return await this.findOne({ kakaoId });
     }
 }
